a0c81dde1e9752d33a3106a75dc113800290c5f1
Frode Hansen <frode.hansen@bekk.no>
2020-05-05 14:30:55 +0200

    Ekstrainfo vedrørende kun søke én gang

--- src/app/pages/intro-page/IntroPage.tsx
+++ src/app/pages/intro-page/IntroPage.tsx
@@ -19,6 +19,7 @@ import IntroCheckList from './IntroCheckList';
 import { useIntl } from 'react-intl';
 import useTilgjengelig from '../../hooks/useTilgjengelig';
 import Lenke from 'nav-frontend-lenker';
+import InfoMessage from '../../components/info-message/InfoMessage';
 
 const bem = bemUtils('introPage');
 
@@ -130,6 +131,19 @@ const IntroPage: React.StatelessComponent = () => {
                                             </p>
                                         </Normaltekst>
                                     </InformationPoster>
+                                </Box>
+                                <Box margin="xl">
+                                    <InfoMessage>
+                                        <strong>Du kan kun sende inn søknaden én gang</strong>. Det vil si at hvis du
+                                        sender inn en søknad med opplysninger som er feil, kan du ikke sende ny søknad
+                                        eller trekke den søknaden du har sendt.
+                                        <p>
+                                            <strong>
+                                                Det er derfor viktig at du kontrollerer at tallene du legger inn er
+                                                riktige.
+                                            </strong>
+                                        </p>
+                                    </InfoMessage>
                                 </Box>
                                 <Box margin="xl">
                                     <ResponsivePanel>
