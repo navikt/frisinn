f4c4422274b90ab3ae510a02eadd28fe20fb1303
Frode Hansen <frode.hansen@bekk.no>
2020-06-08 17:30:29 +0200

    Update tests

--- src/app/__mock__/personligeForetakMock.ts
+++ src/app/__mock__/personligeForetakMock.ts
@@ -1,24 +1,31 @@
 import { apiStringDateToDate } from '@navikt/sif-common-core/lib/utils/dateUtils';
+import { Foretak } from '../types/SoknadEssentials';
+
+const foretak: Foretak = {
+    organisasjonsnummer: '123',
+    navn: 'Foretaket',
+    registreringsdato: apiStringDateToDate('2020-01-01'),
+};
 
 export const PersonligeForetakMock = {
     personligeFortak1998: {
-        foretak: [],
+        foretak: [{ ...foretak, registreringsdato: apiStringDateToDate('1998-01-01') }],
         tidligsteRegistreringsdato: apiStringDateToDate('1998-01-01'),
     },
     personligeFortak2017: {
-        foretak: [],
+        foretak: [{ ...foretak, registreringsdato: apiStringDateToDate('2017-12-31') }],
         tidligsteRegistreringsdato: apiStringDateToDate('2017-12-31'),
     },
     personligeFortak2018: {
-        foretak: [],
+        foretak: [{ ...foretak, registreringsdato: apiStringDateToDate('2018-01-01') }],
         tidligsteRegistreringsdato: apiStringDateToDate('2018-01-01'),
     },
     personligeFortak2019: {
-        foretak: [],
+        foretak: [{ ...foretak, registreringsdato: apiStringDateToDate('2019-01-01') }],
         tidligsteRegistreringsdato: apiStringDateToDate('2019-01-01'),
     },
     personligeFortak2020: {
-        foretak: [],
+        foretak: [{ ...foretak, registreringsdato: apiStringDateToDate('2020-01-01') }],
         tidligsteRegistreringsdato: apiStringDateToDate('2020-01-01'),
     },
 };
