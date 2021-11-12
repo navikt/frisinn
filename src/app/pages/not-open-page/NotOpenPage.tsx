51bb5f9a0369424bd8aa9297bf8780310e6df647
Frode Hansen <frode.hansen@bekk.no>
2021-11-12 12:33:37 +0100

    Endre tekst på NotOpenPage

--- src/app/pages/not-open-page/NotOpenPage.tsx
+++ src/app/pages/not-open-page/NotOpenPage.tsx
@@ -3,16 +3,24 @@ import ErrorPage from '../../components/error-page/ErrorPage';
 import ErrorGuide from '../../components/error-guide/ErrorGuide';
 import { Ingress } from 'nav-frontend-typografi';
 
+const isClosed = false;
+
 const NotOpenPage: React.StatelessComponent<{}> = () => {
     return (
         <ErrorPage pageTitle="Søknaden er ikke tilgjengelig">
-            <ErrorGuide title="Søknaden er ikke tilgjengelig" stillHappy={false}>
-                <Ingress tag="div">
-                    Ordningen opphørte 30. september 2021, men det er lagt frem forslag om at ordningen skal forlenges
-                    ut 2021. Vi oppdaterer informasjonen på denne siden når Stortinget har vurdert forslaget og fattet
-                    et nytt vedtak for ordningen.
-                </Ingress>
-            </ErrorGuide>
+            {isClosed ? (
+                <ErrorGuide title="Søknaden er ikke tilgjengelig" stillHappy={false}>
+                    <Ingress tag="div">
+                        Ordningen opphørte 30. september 2021, men det er lagt frem forslag om at ordningen skal
+                        forlenges ut 2021. Vi oppdaterer informasjonen på denne siden når Stortinget har vurdert
+                        forslaget og fattet et nytt vedtak for ordningen.
+                    </Ingress>
+                </ErrorGuide>
+            ) : (
+                <ErrorGuide title="Søknaden er dessverre ikke tilgjengelig" stillHappy={false}>
+                    <Ingress tag="div">Vi jobber så raskt vi kan med å få den tilgjengelig.</Ingress>
+                </ErrorGuide>
+            )}
         </ErrorPage>
     );
 };
