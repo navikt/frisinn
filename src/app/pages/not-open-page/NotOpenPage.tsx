6379548596635cbece18ddbbcf2cb356fd37e7d1
frodehansen2 <frode.hansen@bekk.no>
2022-05-02 09:42:07 +0200

    Oppdatere tekst om at ordningen er stengt (#465)
    * Oppdatere tekst om at ordningen er stengt

--- src/app/pages/not-open-page/NotOpenPage.tsx
+++ src/app/pages/not-open-page/NotOpenPage.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import ErrorPage from '../../components/error-page/ErrorPage';
+import Box from '@navikt/sif-common-core/lib/components/box/Box';
+import InformationPoster from '@navikt/sif-common-core/lib/components/information-poster/InformationPoster';
+import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { Ingress, Undertittel } from 'nav-frontend-typografi';
 import ErrorGuide from '../../components/error-guide/ErrorGuide';
-import { Ingress } from 'nav-frontend-typografi';
+import ErrorPage from '../../components/error-page/ErrorPage';
 
-const isClosed = false;
+const isClosed = true;
 
 const NotOpenPage: React.StatelessComponent<{}> = () => {
     return (
-        <ErrorPage pageTitle="Søknaden er ikke tilgjengelig">
+        <ErrorPage pageTitle="Søknaden er nå ikke tilgjengelig">
             {isClosed ? (
-                <ErrorGuide title="Søknaden er ikke tilgjengelig" stillHappy={false}>
-                    <Ingress tag="div">
-                        Ordningen opphørte 30. september 2021, men det er lagt frem forslag om at ordningen skal
-                        forlenges ut 2021. Vi oppdaterer informasjonen på denne siden når Stortinget har vurdert
-                        forslaget og fattet et nytt vedtak for ordningen.
-                    </Ingress>
-                </ErrorGuide>
+                <InformationPoster>
+                    <Undertittel>
+                        Søknad om kompensasjon for tapt inntekt for selvstendig næringsdrivende (ENK, DA/ANS) og
+                        frilansere
+                    </Undertittel>
+
+                    <Box margin="xl" padBottom="l">
+                        <AlertStripeInfo>
+                            <strong>Ordningen opphørte 31. mars 2022</strong>
+                            <br /> Det betyr at det nå ikke lenger er mulig å søke kompensasjon gjennom denne ordningen.
+                        </AlertStripeInfo>
+                    </Box>
+                </InformationPoster>
             ) : (
                 <ErrorGuide title="Søknaden er dessverre ikke tilgjengelig" stillHappy={false}>
                     <Ingress tag="div">Vi jobber så raskt vi kan med å få den tilgjengelig.</Ingress>
